b47b112f2069267423e27d78fd1097da32117147
Andy Golubev <30592974+andygolubev@users.noreply.github.com>
2026-01-08 16:47:24 +0200

    Implement number guessing logic in main.rs, including random number generation and comparison feedback for user guesses.

--- rustbook-examples/chapter02/guess_game/src/main.rs
+++ rustbook-examples/chapter02/guess_game/src/main.rs
@@ -1,4 +1,6 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("guess the number!");
@@ -6,10 +8,20 @@ fn main() {
     println!("Please input your guess.");
 
     let mut guess = String::new();
+    let secret_number = rand::rng().random_range(1..=100);
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+    println!("The secret number is: {secret_number}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
